4c771d593692c4ef53f9fabf2e7aa9aae53a5459
baldock92 <george_baldock@hotmail.co.uk>
2022-07-27 13:08:10 +0100

    add about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layouts/Layout'
 import Home from './components/Home/Home.js'
+import About from './components/About/About.js'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
