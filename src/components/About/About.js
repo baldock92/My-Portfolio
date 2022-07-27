fc8ac84a3c65302507b62e2eb0205060330d7062
baldock92 <george_baldock@hotmail.co.uk>
2022-07-27 13:18:55 +0100

    add loading bar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,6 +11,7 @@ import {
   faNode,
   faGithub,
 } from '@fortawesome/free-brands-svg-icons'
+import {Loader} from "react-loaders"
 
 export default function About() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -22,55 +23,58 @@ export default function About() {
   }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I'm a very ambitious full-stack software developer, looking for a role
-          where I can work with existing and new technologies on challenging and
-          diverse projects.
-        </p>
-        <p>
-          I'm a confident individual with great communication skills, and love
-          working as part of a team.
-        </p>
-        <p>
-          Outside of work I love sports, mainly golf football and poker
-          (although perhaps not a sport!) as well as a general love for the
-          great outdoors. Travel is my biggest joy in life, and it's my life
-          ambition to see as much of the World as possible, with a slice of
-          pizza in one hand as I do.
-        </p>
-      </div>
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faNode} color="#87bf00" />
-          </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious full-stack software developer, looking for a
+            role where I can work with existing and new technologies on
+            challenging and diverse projects.
+          </p>
+          <p>
+            I'm a confident individual with great communication skills, and love
+            working as part of a team.
+          </p>
+          <p>
+            Outside of work I love sports, mainly golf football and poker
+            (although perhaps not a sport!) as well as a general love for the
+            great outdoors. Travel is my biggest joy in life, and it's my life
+            ambition to see as much of the World as possible, with a slice of
+            pizza in one hand as I do.
+          </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faNode} color="#87bf00" />
+            </div>
 
-          <div className="face4">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face1">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGithub} />
+            <div className="face4">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face1">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" /> 
+    </>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,8 @@ import Gtitle from '../../assets/images/G-logo.png'
 import { Link } from 'react-router-dom'
 import './Home.scss'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
-import Logo from "../Logo/Logo"
+import Logo from '../Logo/Logo';
+import {Loader} from "react-loaders"
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -36,33 +37,36 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={Gtitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Full stack software developer</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={Gtitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Full stack software developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
