de3b32f821b71aa924465826981803d051530386
baldock92 <george_baldock@hotmail.co.uk>
2022-07-28 15:43:18 +0100

    add redirects file, link erroneous endpoints to homepage

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </>
